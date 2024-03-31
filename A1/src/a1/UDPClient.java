1281ced3517bb9d911ca3de948deadc83f5dbb8d
Joshua <Joshua@192.168.0.24>
2024-04-01 00:54:32 +1000

    Corrected UDP Client and Server classes Added minor modifications to TCP classes Most output is generated for now Need to fix error with TCP client only sending one member per run

--- A1/src/a1/UDPClient.java
+++ A1/src/a1/UDPClient.java
@@ -1,9 +1,58 @@
 package a1;
 
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author Joshua
  */
 public class UDPClient {
-    
+
+    public static void main(String args[]) {
+
+        //The first argument is the message to send to the server.
+        //The second argument is the name of the server.
+        DatagramSocket aSocket = null;
+        try {
+
+            //Create a UDP socket
+            aSocket = new DatagramSocket();
+
+            String msg = "memberlistObject";
+            //Prepare the message to send to the server
+            byte[] m = msg.getBytes();
+            InetAddress aHost = InetAddress.getByName("localhost");
+
+            //Agreed port
+            int serverPort = 2275;
+
+            //Create a UDP datagram
+            DatagramPacket request = new DatagramPacket(m, msg.length(), aHost, serverPort);
+
+            //Send the request
+            aSocket.send(request);
+
+            //Prepare a buffer to receive the reply from the server
+            byte[] buffer = new byte[1000];
+
+            //Waiting for reply
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+            aSocket.receive(reply);
+
+            //Display the reply
+            String response = new String(reply.getData(), 0, reply.getLength());
+            System.out.println("Server Response: \n" + response);
+
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        } finally {
+            if (aSocket != null) {
+                aSocket.close();
+            }
+        }
+    }
 }
--- A1/src/a1/UDPServer.java
+++ A1/src/a1/UDPServer.java
@@ -1,9 +1,37 @@
 package a1;
 
+import java.net.*;
+import java.io.*;
+
 /**
  *
  * @author Joshua
  */
 public class UDPServer {
-    
+
+    public static void main(String args[]) {
+        DatagramSocket aSocket = null;
+        int port = 2275;
+        try {
+            aSocket = new DatagramSocket(port);
+            byte[] buffer = new byte[1000];
+            while (true) {
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                aSocket.receive(request);
+                System.out.println("Client Request: " + new String(request.getData(), 0, request.getLength()));
+                String response = "|First Name \t |Last Name \t |Address \t |Phone Number \t | \n==================================================================";
+                byte[] r = response.getBytes();
+                DatagramPacket reply = new DatagramPacket(r, response.length(),request.getAddress(), request.getPort());
+                aSocket.send(reply);
+            }
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        } finally {
+            if (aSocket != null) {
+                aSocket.close();
+            }
+        }
+    }
 }
